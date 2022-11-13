792ca4af348067718c2441e51b7381f70df4ee9f
randomgeekdom <randomgeekdom@gmail.com>
2022-11-13 15:04:56 -0600

    attempting design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,8 +11,12 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 
 import { CharacterSheetComponent } from './character-sheet/character-sheet.component';
@@ -30,7 +34,11 @@ import { CharacterSheetComponent } from './character-sheet/character-sheet.compo
     MatMenuModule,
     MatFormFieldModule,
     MatSelectModule,
-    MatButtonModule
+    MatButtonModule,
+    MatGridListModule,
+    MatCardModule,
+    MatInputModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/character-sheet/character-sheet.component.ts
+++ src/app/character-sheet/character-sheet.component.ts
@@ -9,9 +9,15 @@ import { CharacterSheetInitializerService } from '../services/character-sheet-in
 })
 export class CharacterSheetComponent implements OnInit {
   CharacterSheet: CharacterSheet;
+  breakpoint = 6;
   constructor(characterSheetInitializer: CharacterSheetInitializerService) {
     this.CharacterSheet = characterSheetInitializer.Initialize();
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {   this.breakpoint = (window.innerWidth <= 400) ? 1 : 3;
+  }
+
+  onResize(event: any) {
+    this.breakpoint = (event.target.innerWidth <= 800) ? 1 : 3;
+  }
 }
